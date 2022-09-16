dcc9e25578ca6b2ac7697440a01c44d75dff7ddf
benthemobileguy <benbright065@gmail.com>
2022-09-16 10:58:01 +0100

    initialised files

---
+++ android/app/src/main/kotlin/com/example/flutter_iot_ben/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_iot_ben
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iot_ben/pages/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter IOT Ben',
+      theme: ThemeData(
+          fontFamily: "Poppins",
+          sliderTheme: const SliderThemeData(
+            trackShape: RectangularSliderTrackShape(),
+            trackHeight: 2.5,
+            thumbShape: RoundSliderThumbShape(enabledThumbRadius: 8.0),
+            overlayShape: RoundSliderOverlayShape(overlayRadius: 15.0),
+          )),
+      home: const HomePage(),
+    );
+  }
+}
